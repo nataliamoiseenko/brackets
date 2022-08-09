681eae6e00b37dbd43d42d550aa262391e435131
nataliamoiseenko <natallia.moiseenko@gmail.com>
2022-08-09 15:45:06 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const parsedBracketsConfig = bracketsConfig.map((item) => {
+    return `${item[0]}${item[1]}`;
+  });
+
+  let prevLength = str.length;
+
+  while (str.length) {
+    parsedBracketsConfig.forEach((item) => {
+      str = str.replaceAll(item, "");
+    });
+
+    if (str.length === prevLength) {
+      return false;
+    }
+
+    prevLength = str.length;
+  }
+
+  return true;
 }
